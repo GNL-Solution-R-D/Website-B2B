ee3b2b2759fd2dce4f20b6e8801e91d70ba50197
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-08-04 00:25:39 +0200

    Update cgu.php

--- pages/cgu.php
+++ pages/cgu.php
@@ -22,16 +22,12 @@ $page_titre = 'Conditions Générales d’Utilisation';
 </head>
 <body>
 
-<header class="legal-header">
-    <div class="inner">
-        <a class="brand" href="<?= e($LEGAL['site_url']) ?>"><?= e($LEGAL['site_nom']) ?></a>
-        <nav aria-label="Pages légales">
-            <a href="cgu.php" aria-current="page">CGU</a>
-            <a href="cgv.php">CGV</a>
-            <a href="politique-de-confidentialite.php">Confidentialité</a>
-        </nav>
-    </div>
-</header>
+<?php
+// Pied de page commun (identique à index.php)
+if (is_readable('../include/header.php')) {
+    include '../include/header.php';
+}
+?>
 
 <div class="legal-wrap">
 
