081281aecdef2bd4b6d1d6a96469225d863c7b67
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-08-03 21:41:01 +0200

    Update product-configuration.php

--- pages/product-configuration.php
+++ pages/product-configuration.php
@@ -994,8 +994,9 @@ document.addEventListener('DOMContentLoaded',function(){
 
 <?php
 // Pied de page commun (identique à index.php)
-
-    include './include/footer.php';
+if (is_readable('../include/footer.php')) {
+    include '../include/footer.php';
+}
 ?>
 </div><!-- .wp-site-blocks -->
 
